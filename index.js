f72ae3dbe383d4fd4704feaac011a7cafa46a7fd
Bindu Prakash <bindu2305@gmail.com>
2019-03-07 06:27:58 +0000

    Step 3 using methods completed

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function(){
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function(){
+      this.complete = true;
+    }
   };
   return task;
-}
-
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-function completeTask(task) {
-  task.complete = true;
 }
 
 const task1 = newTask("Clean Cat Litter", "Take all the poop out of the litter box");
 const task2 = newTask("Do Laundry", ":(");
 const tasks = [task1, task2];
 
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
